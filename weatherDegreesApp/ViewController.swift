94d3c38e44851bab1bedd70b36e7476003440bdc
jackjoseph7447 <jackjoseph7447@gmail.com>
2019-11-04 12:00:29 -0500

    Initial Commit

---
+++ weatherDegreesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherDegreesApp
+//
+//  Created by  on 11/4/19.
+//  Copyright © 2019 JacksApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
