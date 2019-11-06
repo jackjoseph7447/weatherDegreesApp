619e45a2bbda473cbad7b83f0dcf62c6f8b2c250
jackjoseph7447 <jackjoseph7447@gmail.com>
2019-11-06 12:04:37 -0500

    minor changes

--- weatherDegreesApp/ViewController.swift
+++ weatherDegreesApp/ViewController.swift
@@ -8,11 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    var F : Double = 0
+    var C : Double = 0
+    
+    
+//    var F : Double = 0
+//    var C : Double = 0
+//
+//    func calculateCelcius()
+//    {
+//      F = 9.0/5.0*C + 32.0
+//    }
+//    C = 12
+//    calculateCelcius()
+//
+//    print("It is " + String(F) + " degrees fahrenheit.")
+//
+//    var A : Double = 0 //Fahrenheit
+//    var B : Double = 0 //Ceclius
+//
+//    func calculateF()
+//    {
+//    B = ((A - 32.0)*5.0/9.0)
+//    }
+//
+//    A = 53.6
+//    calculateF()
+//    print("It is " + String(B) + " degrees celcius")
+//
+//    var K : Double = 0
+//
+//    func calculateK()
+//    {
+//    K = (9.0/5.0*B + 32.0) + 273.15
+//    }
+//
+//    calculateK()
+//
+//    print("It is " + String(K) + " kelvin")
+
+    
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
+
+
+
     }
 
 
