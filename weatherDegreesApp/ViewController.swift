d837979c3738e49abb17d2cce49c833f3afc6d98
jackjoseph7447 <jackjoseph7447@gmail.com>
2019-11-07 11:06:03 -0500

    Added new view controllers for the different units. created new files for views as well.

--- weatherDegreesApp/ViewController.swift
+++ weatherDegreesApp/ViewController.swift
@@ -11,44 +11,10 @@ import UIKit
 class ViewController: UIViewController
 {
 
-    var F : Double = 0
-    var C : Double = 0
+    @IBOutlet weak var welcomeLabel: UILabel!
     
     
-//    var F : Double = 0
-//    var C : Double = 0
-//
-//    func calculateCelcius()
-//    {
-//      F = 9.0/5.0*C + 32.0
-//    }
-//    C = 12
-//    calculateCelcius()
-//
-//    print("It is " + String(F) + " degrees fahrenheit.")
-//
-//    var A : Double = 0 //Fahrenheit
-//    var B : Double = 0 //Ceclius
-//
-//    func calculateF()
-//    {
-//    B = ((A - 32.0)*5.0/9.0)
-//    }
-//
-//    A = 53.6
-//    calculateF()
-//    print("It is " + String(B) + " degrees celcius")
-//
-//    var K : Double = 0
-//
-//    func calculateK()
-//    {
-//    K = (9.0/5.0*B + 32.0) + 273.15
-//    }
-//
-//    calculateK()
-//
-//    print("It is " + String(K) + " kelvin")
+    
 
     
     
@@ -57,7 +23,7 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
+        welcomeLabel.text = "Welcome to the weather calculator app! \n Please select what you would like to calculate"
 
 
 
