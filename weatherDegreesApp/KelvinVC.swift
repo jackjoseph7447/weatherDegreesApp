1b4a708d5ded96e9d3fd73731a12490a43577733
AirBarrett <thomasbarrett512001@gmail.com>
2019-11-19 11:32:36 -0500

    Fixed stuff

--- weatherDegreesApp/KelvinVC.swift
+++ weatherDegreesApp/KelvinVC.swift
@@ -11,29 +11,57 @@ import UIKit
 class KelvinVC: UIViewController
 {
     @IBOutlet weak var inputText: UITextField!
-    @IBOutlet weak var resultLabel: UILabel!
+ 
+    @IBOutlet weak var celciusResult: UILabel!
     
+    @IBOutlet weak var farhenhitResult: UILabel!
+    @IBOutlet weak var farhenhitText: UITextField!
+    @IBOutlet weak var celcuisText: UITextField!
     var K : Double = 0
     var B : Double = 0
+    var F : Double = 0
+    var A : Double = 0
+    var C : Double = 0
     
     func calculateK()
         {
-            K = (9.0/5.0*B + 32.0) + 273.15
-            B = Double(inputText.text!)!
+            K = B + 273.15
+            B = Double(celcuisText.text!)!
 
         }
+    func calculateKelvin()
+    {
+        A = Double(farhenhitText.text!)!
+        C = (A-32.0) * 5.0/9.0 + 273.15
+    }
     
     @IBAction func calcButton(_ sender: Any)
     {
+        if celcuisText.text != "" &&
+        farhenhitText.text == ""
+        {
+            calculateK()
+            celciusResult.text = "It is \(K) kelvin"
+        }
+        if celcuisText.text != "" &&
+        farhenhitText.text == ""
+        {
+            calculateK()
+            farhenhitResult.text = "It is \(C) kelvin"
+        }
+        calculateKelvin()
         calculateK()
-        resultLabel.text = "It is \(K) kelvin"
+        farhenhitResult.text = "It is \(K) kelvin"
+        celciusResult.text = "It is \(C) kelvin"
     }
     
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        B = Double(inputText.text!)!
+        celciusResult.text = ""
+        farhenhitResult.text = ""
+   
 
 
     }
